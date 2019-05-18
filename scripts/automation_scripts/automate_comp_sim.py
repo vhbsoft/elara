39694e3e257f2e1a0f92fe318840e15298c75d51
Mario Anthony Garcia <40250673+mg424@users.noreply.github.com>
2019-05-18 14:43:30 -0700

    Added comments
    Added comments for clarity.

--- scripts/automation_scripts/automate_comp_sim.py
+++ scripts/automation_scripts/automate_comp_sim.py
@@ -1,21 +1,34 @@
-#Automated compression simulation for ./waf commands in NS-3
-import csv
+#Script by Mario A Garcia 
+#Github: https://github.com/mg424
+#Created @CSUN in Summer 2018
+
+#This script is used to automate the waf commands that run compression simulation in NS-3.
+#This will generate a .csv file with the following format
+#
+#	 _______________________________________________________________________	
+#	|		Packet id 		|		Timestamp or -1(if packet was lost)		|
+#	|_______________________|_______________________________________________|
+#	|		  XXXX			|					-1							|
+#	|_______________________|_______________________________________________|	
+#
+
 import os
 
 #Example run of compression sim using waf commands for reference
+#This example has every parameter that is possible present.
 #./waf --run "4node-compression --outputFile=./test.dat --entropy=h 
 #--numPackets=1000  --packetSize=1024 --interPacketTime=.0000001 --s0p0Delay=30ms 
 #--p0p1Delay=30ms --p1r0Delay=30ms --s0p0DataRate=5Mbps --p0p1DataRate=4Mbps 
 #--p1r0DataRate=5Mbps --queueMode=b --s0QueueSize=655350000 --p0QueueSize=655350000 
 #--p1QueueSize=655350000  --compression=1"
 
-#Directory where waf commands will compile
+#Directory where NS-3 is located waf commands can be run here.
 pathTo = "/home/mario/repos/ns-allinone-3.14.1/ns-3.14.1"
 
 #This command changes the directory to where the waf commands are enabled
 os.chdir(pathTo)
 
-#The below command is how to execute the compression sim using a ./waf command
+#The below command is a simple example waf command of how to execute the compression simulation.
 #os.system('./waf --run "compression-sim --numPackets=50 --packetSize=500"')
 
 compression_args = {}
@@ -27,10 +40,10 @@ compression_args['entropy'] = 'h'
 compression_args['packetSize'] = '1100'
 compression_args['numPackets'] = '500'
 compression_args['interPacketTime'] = '0.00000001'
-compression_args['s0QueueSize'] = '2000'
-compression_args['p0QueueSize'] = '4800' #Compressor Transmission queue size
-compression_args['p1QueueSize'] = '2000'
-compression_args['p0p1DataRate'] = '1Mbps' #Compression link
+compression_args['s0QueueSize'] = '655350000'
+compression_args['p0QueueSize'] = '75' #Compressor Transmission queue size
+compression_args['p1QueueSize'] = '655350000'
+compression_args['p0p1DataRate'] = '2Mbps' #Compression link
 compression_args['s0p0DataRate'] = '5Mbps' 
 compression_args['p1r0DataRate'] = '5Mbps'
 compression_args['s0p0Delay']='30ms'
@@ -38,10 +51,10 @@ compression_args['p0p1Delay']='30ms'
 compression_args['p1r0Delay']='30ms'
 
 #change values here to alter the name of the first half outputfile
-compression_args['outputFile'] = './test_' 
+compression_args['outputFile'] = './loss_' 
 
 #helper array to loop through bandwidths
-bands = [1, 4 , 5, 6]
+bands = [2, 4 , 5, 6]
 counter = 0						#for str(bands[counter]) and test_args['bandwidth'] 
 outcome = []
 numPackets = 500
@@ -52,16 +65,16 @@ numPackets = 500
 while True:
 	if counter != 0:
 		numPackets = 500
-	if counter > 3:
+	if counter > 0:
 		break
-	for y in range(500, 6500, 500):
-		if numPackets > 6000:
+	for y in range(500, 10500, 500):
+		if numPackets > 10000:
 			break
 
 		#high entropy turn
 		comp_cmds = './waf --run "'
 		comp_cmds += 'compression-sim'
-		comp_cmds += ' --outputFile=' + compression_args['outputFile'] + str(bands[counter]) +'Mbps_' + '_num_of_packets_' + str(numPackets) + '_H_' + '.csv'
+		comp_cmds += ' --outputFile=' + compression_args['outputFile'] + str(bands[counter]) +'Mbps' + '_num_of_packets_' + str(numPackets) + '_H_' + '.csv'
 		comp_cmds += ' --packetSize=' + compression_args['packetSize']
 		comp_cmds += ' --s0p0DataRate=' + compression_args['s0p0DataRate']
 		comp_cmds += ' --p1r0DataRate=' + compression_args['p1r0DataRate']
@@ -77,7 +90,7 @@ while True:
 		#Low entropy turn
 		comp_cmds = './waf --run "'
 		comp_cmds += 'compression-sim'
-		comp_cmds += ' --outputFile=' + compression_args['outputFile'] + str(bands[counter]) +'Mbps_' + '_num_of_packets_' + str(numPackets) + '_L_' + '.csv'
+		comp_cmds += ' --outputFile=' + compression_args['outputFile'] + str(bands[counter]) +'Mbps' + '_num_of_packets_' + str(numPackets) + '_L_' + '.csv'
 		comp_cmds += ' --packetSize=' + compression_args['packetSize']
 		comp_cmds += ' --s0p0DataRate=' + compression_args['s0p0DataRate']
 		comp_cmds += ' --p1r0DataRate=' + compression_args['p1r0DataRate']
